d0c0f1648afaecbaed239f4e8755a4f0826b0743
OUAIDA Yassine <63479028+YassineOUAIDA@users.noreply.github.com>
2024-04-17 17:36:24 +0100

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,10 @@ import streamlit as st
 AUTHOR = 'OUAIDA YASSINE'
 
 
+
+
 st.write(f'This app was built by {AUTHOR}')
+st.text_input('Your email here')
 st.date_input('Entrer votre date de naissance')
 msg = st.chat_input('Entrer votre message ici : ')
 
