896e0a327e1eb95ec45cdadddd76f8e5769e9747
OUAIDA Yassine <63479028+YassineOUAIDA@users.noreply.github.com>
2024-04-19 20:57:57 +0100

    Update index.py

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ import numpy as np
 
 st.sidebar.write('Welcome to this sidebar')
 st.sidebar.text_input('Enter your data : ')
+st.sidebar.button('Click here')
 st.write(f'This app was built by {AUTHOR}')
 st.text_input('Your email here')
 st.date_input('Entrer votre date de naissance')
