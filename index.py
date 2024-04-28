c7a12c3eb442f9af0014d9e025017d06869b4749
OUAIDA Yassine <63479028+YassineOUAIDA@users.noreply.github.com>
2024-04-28 21:51:53 +0100

    Update index.py

--- index.py
+++ index.py
@@ -3,10 +3,18 @@ AUTHOR = 'OUAIDA YASSINE'
 import pandas as pd
 import numpy as np
 
-html_string = """<script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9450187741597742'
-     crossorigin='anonymous'></script>"""
+my_js = """
+alert("Hola mundo");
+"""
 
-st.markdown(html_string, unsafe_allow_html=True)
+# Wrapt the javascript as html code
+my_html = f"<script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9450187741597742'
+     crossorigin='anonymous'>{my_js}</script>"
+
+# Execute your app
+st.title("Javascript example")
+html(my_html)
+
 
 st.sidebar.write('Welcome to this sidebar')
 st.sidebar.text_input('Enter your data : ')
